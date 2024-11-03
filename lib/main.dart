aadf87db53ae791c85b88b2b013943685e0bfc19
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-03 16:34:31 +0530

    Create HomeScreen Function

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,17 @@
 import 'package:flutter/material.dart';
 
-/*************  ✨ Codeium Command ⭐  *************/
-/// The application entrypoint.
-///
-/// This function is the first code to run when the application is started.
-/// It configures the top-level [MaterialApp] and [Scaffold] to display a
-/// centered "Hello World" message in 50-point white text on a pale purple
-/// background with 33% opacity.
-/******  27ddfb2d-7012-47d0-9218-27bb56048bcb  *******/
 void main() {
   runApp(MaterialApp(
-    home: Scaffold(
+    home: HomeScreen(),
+   
+  ));
+}
+
+
+class HomeScreen extends StatelessWidget {
+  @override
+  Widget build( context) {
+    return const Scaffold(
       backgroundColor: Color.fromARGB(133, 173, 109, 241),
       body: Center(
         child: Text(
@@ -18,14 +19,6 @@ void main() {
           style: TextStyle(color: Colors.white, fontSize: 50),
         ),
       ),
-    ),
-  ));
-}
-
-
-class HomeScreen extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return Container();
+    );
   }
 }
