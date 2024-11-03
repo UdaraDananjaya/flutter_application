7dc025e9da8af2f6280f5294f330c608b7c84290
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-03 17:27:02 +0530

    Pass Variable

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_application/styled_tex.dart';
 
+const beginAlignment = Alignment.topRight;
+const endAlignment = Alignment.bottomLeft;
+//Widget? endAlignment = null;
+
 class HomeScreen extends StatelessWidget {
   const HomeScreen({super.key});
   @override
@@ -10,14 +14,10 @@ class HomeScreen extends StatelessWidget {
         // backgroundColor: Color.fromARGB(133, 173, 109, 241),
         body: Container(
           decoration: const BoxDecoration(
-              gradient: LinearGradient(
-            colors: [
-              Color.fromARGB(255, 0, 98, 255),
-              Color.fromARGB(255, 38, 0, 255),
-            ],
-            begin: Alignment.topRight,
-            end:Alignment.bottomLeft
-          )),
+              gradient: LinearGradient(colors: [
+            Color.fromARGB(255, 0, 98, 255),
+            Color.fromARGB(255, 38, 0, 255),
+          ], begin: beginAlignment, end: endAlignment)),
           child: const StyledTex(),
         ),
       ),
--- lib/styled_tex.dart
+++ lib/styled_tex.dart
@@ -1,12 +1,14 @@
 import 'package:flutter/material.dart';
 
+const parameterText = "Welcome";
+
 class StyledTex extends StatelessWidget {
   const StyledTex({super.key});
   @override
   Widget build(context) {
     return const Center(
       child: Text(
-        "Hello World",
+        parameterText,
         style: TextStyle(color: Colors.white, fontSize: 50),
       ),
     );
