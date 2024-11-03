405393e5512cf3e3de4e6aeda69d5f311e140c42
Udara Dananjaya <udarapaymedia@gmail.com>
2024-11-03 16:27:08 +0530

    Change Colors and Add font Styless

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
+/*************  ✨ Codeium Command ⭐  *************/
+/// The application entrypoint.
+///
+/// This function is the first code to run when the application is started.
+/// It configures the top-level [MaterialApp] and [Scaffold] to display a
+/// centered "Hello World" message in 50-point white text on a pale purple
+/// background with 33% opacity.
+/******  27ddfb2d-7012-47d0-9218-27bb56048bcb  *******/
 void main() {
   runApp(MaterialApp(
     home: Scaffold(
-      backgroundColor: Color.fromRGBO(255, 0, 0, 1), 
+      backgroundColor: Color.fromARGB(133, 173, 109, 241),
       body: Center(
-        child: Text("Hello World"),
+        child: Text(
+          "Hello World",
+          style: TextStyle(color: Colors.white, fontSize: 50),
+        ),
       ),
     ),
   ));
